864d2968a01b01a85c9a87507e02e25c2808d47c
samcurteis <samcurteis@gmail.com>
2023-01-27 09:30:05 +0000

    deploying

--- makeseeds.sh
+++ makeseeds.sh
@@ -3,3 +3,15 @@ python manage.py dumpdata poems --output poems/seeds.json --indent=2;
 
 echo "creating jwt_auth/seeds.json"
 python manage.py dumpdata jwt_auth --output jwt_auth/seeds.json --indent=2;
+
+echo "creating authors/seeds.json"
+python manage.py dumpdata authors --output authors/seeds.json --indent=2;
+
+echo "creating comments/seeds.json"
+python manage.py dumpdata comments --output comments/seeds.json --indent=2;
+
+echo "creating poems/seeds.json"
+python manage.py dumpdata poems --output poems/seeds.json --indent=2;
+
+echo "creating posts/seeds.json"
+python manage.py dumpdata posts --output posts/seeds.json --indent=2;
--- seed.sh
+++ seed.sh
@@ -13,3 +13,13 @@ python manage.py loaddata jwt_auth/seeds.json
 
 echo "inserting poems"
 python manage.py loaddata poems/seeds.json
+
+echo "inserting authors"
+python manage.py loaddata authors/seeds.json
+
+echo "inserting comments"
+python manage.py loaddata comments/seeds.json
+
+echo "inserting posts"
+python manage.py loaddata posts/seeds.json
+
