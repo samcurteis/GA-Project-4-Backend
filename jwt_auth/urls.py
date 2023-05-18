6ad9fee2e9436e2d8186b7279e6d2e0e6e8ce2d6
samcurteis <samcurteis@gmail.com>
2023-05-18 11:19:55 +0100

    set up basic test for register

--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -3,8 +3,8 @@ from .views import RegisterView
 from .views import LoginView, UserListView, UserDetailView, UserSearchView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', LoginView.as_view(), name="login"),
     path('', UserListView.as_view()),
     path('<int:pk>/', UserDetailView.as_view()),
     path('search/', UserSearchView.as_view())
